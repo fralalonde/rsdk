6fc2b73b652e115dc63cb898cec4e832e3e2dbb6
Francis Lalonde <francis.lalonde@onomatic.com>
2024-10-28 23:31:26 -0400

    Fix gha win create dest dir.2

--- build/windows/Module-Template.ps1
+++ build/windows/Module-Template.ps1
@@ -2,10 +2,10 @@
 
 param (
     [Parameter(Mandatory = $true)]
-    [string]$SourceDirectory,   # Path to the directory containing template files (e.g., .psd1, .psm1)
+    [string]$SourceDir,
 
     [Parameter(Mandatory = $true)]
-    [string]$DestinationDirectory, # Path to the directory where templates will be copied
+    [string]$DestinationDir,
 
     [Parameter(Mandatory = $true)]
     [string]$ExePath            # Path to the rsdk.exe binary
@@ -13,19 +13,19 @@ param (
 
 # Copy all module template files from the source to the destination, excluding Rsdk.psm1
 # Ensure the destination path and any parent directories exist
-if (-not (Test-Path -Path $destinationPath)) {
-    Write-Host "Creating directory $destinationPath and its parent directories"
-    New-Item -ItemType Directory -Path $destinationPath -Force
+if (-not (Test-Path -Path $DestinationDir)) {
+    Write-Host "Creating directory $DestinationDir and its parent directories"
+    New-Item -ItemType Directory -Path $DestinationDir -Force
 }
-Write-Host "Copying module template files from $SourceDirectory to $destinationPath"
-Copy-Item -Path "$SourceDirectory\*" -Destination $destinationPath -Recurse -Exclude "Rsdk.psm1"
+Write-Host "Copying module template files from $SourceDir to $DestinationDir"
+Copy-Item -Path "$SourceDir\*" -Destination $DestinationDir -Recurse -Exclude "Rsdk.psm1"
 
 # Get the absolute path of the rsdk executable
 $rsdkBinarySource = [System.IO.Path]::GetFullPath($ExePath)
 
 # Generate rsdk.psm1 from template with the correct path to rsdk.exe
-$psm1TemplatePath = Join-Path -Path $SourceDirectory -ChildPath "Rsdk.psm1"
-$psm1DestinationPath = Join-Path -Path $destinationPath -ChildPath "Rsdk.psm1"
+$psm1TemplatePath = Join-Path -Path $SourceDir -ChildPath "Rsdk.psm1"
+$psm1DestinationPath = Join-Path -Path $DestinationDir -ChildPath "Rsdk.psm1"
 $rsdkPathEscaped = $rsdkBinarySource
 
 Write-Host "Generating rsdk.psm1 with rsdk.exe path $rsdkPathEscaped"
@@ -33,4 +33,4 @@ $templateContent = Get-Content -Path $psm1TemplatePath -Raw
 $updatedContent = $templateContent -replace 'PUT_RSDK_PATH_HERE', $rsdkPathEscaped
 Set-Content -Path $psm1DestinationPath -Value $updatedContent
 
-Write-Host "Templates successfully copied and configured in $destinationPath"
+Write-Host "Templates successfully copied and configured in $DestinationDir"
